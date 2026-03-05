3a1a1cde44e5aeb857c05e46a8f371cb4d7a76a3
nusratjahan7 <nusratjahan77222@gmail.com>
2026-03-05 20:46:40 +0600

    Loading spinner, click category button to fetch trees by category id by API

--- script.js
+++ script.js
@@ -2,21 +2,56 @@
 const categoriesContainer = document.getElementById("categories-container");
 const treesContainer = document.getElementById("trees-container");
 const loadingSpinner = document.getElementById("loading-spinner");
+const allTreesBtn = document.getAnimations("all-trees-btn");
 
 // step - 1
 async function loadCategories(){
    // async await
     const res = await fetch("https://openapi.programming-hero.com/api/categories");
     const data = await res.json();
-    console.log(data);
+    // console.log(data);
     data.categories.forEach(category => {
-        console.log(category);
+        // console.log(category);
         const btn = document.createElement("button");
         btn.className = "btn btn-wide";
         btn.textContent = category.category_name;
+        btn.onclick = () => selectCategory(category.id, btn);
         categoriesContainer.appendChild(btn);
     });
 };
+
+// step - 6
+async function selectCategory(categoryId, btn){
+    // console.log(categoryId, btn); 
+    showLoading(); 
+    const allBtn = document.querySelectorAll("#categories-container button, #all-trees-btn");
+    allBtn.forEach(btn => {
+        btn.classList.remove("btn-success");
+        btn.classList.add("btn-soft");
+    });
+    btn.classList.add("btn-success");
+    btn.classList.remove("btn-soft");  
+    
+    const res = await fetch('https://openapi.programming-hero.com/api/category/${categoryId}');
+    const data = await res.json();
+    console.log(data);
+    displayTrees(data.plants);
+    hideLoading();
+};
+
+// step - 7
+allTreesBtn.addEventListener("click", () => {
+    const allButtons = document.querySelectorAll("#categories-container button, #all-trees-btn");
+     allButtons.forEach((btn) => {
+    btn.classList.remove("btn-success");
+    btn.classList.add("btn-soft");
+  });
+
+  allTreesBtn.classList.add("btn-success");
+  allTreesBtn.classList.remove("btn-soft");
+
+  loadAllTrees();
+})
 
 // step - 4 
 function showLoading(){
@@ -39,9 +74,9 @@ async function loadAllTrees() {
 
 // step - 3
 function displayTrees(trees) {
-    console.log(trees);
+    // console.log(trees);
     trees.forEach((tree) => {
-        console.log(tree);
+        // console.log(tree);
         const card = document.createElement("div");
         card.className ="card bg-base-100 w-full shadow-sm";
         card.innerHTML = `
